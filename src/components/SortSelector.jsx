a71a193b5260d410243cac5df9aa786d6bf31874
Nimira43 <n.rabone72@aol.com>
2025-12-15 04:04:25 +0000

    Installed React Router.

--- src/components/SortSelector.jsx
+++ src/components/SortSelector.jsx
@@ -10,10 +10,10 @@ const SortSelector = ({ sortBy, onSortChange }) => {
         }
       >
         <option value='market_cap_desc'>
-          Market Cap  (High to Low)
+          Market Cap (High to Low)
         </option>
         <option value='market_cap_asc'>
-          Market Cap  (Low to High)
+          Market Cap (Low to High)
         </option>
         <option value='price_desc'>
           Price (High to Low)
