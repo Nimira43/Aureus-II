6677b3f39f6443846dbfd7e59646a1c44986697b
Nimira43 <n.rabone72@aol.com>
2025-12-25 16:26:20 +0000

    About page -1

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,7 @@ const AboutPage = () => {
   return ( 
     <div className='about'>
       <h1>About Aureus II</h1>
-      <p></p>
+      <p>Aureus II provides fast, reliable insight into the cryptocurrency market using real-time data from CoinGecko. It transforms complex financial information into clear, accessible metrics anyone can understand.</p>
       <p></p>
     </div>
    )
