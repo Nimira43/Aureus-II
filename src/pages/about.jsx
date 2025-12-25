b5bdd240fe831c006748e65b218f64bdd0a68df3
Nimira43 <n.rabone72@aol.com>
2025-12-25 16:27:22 +0000

    About Page -2

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,6 +3,9 @@ const AboutPage = () => {
     <div className='about'>
       <h1>About Aureus II</h1>
       <p>Aureus II provides fast, reliable insight into the cryptocurrency market using real-time data from CoinGecko. It transforms complex financial information into clear, accessible metrics anyone can understand.</p>
+      <p>Designed for traders, learners, and enthusiasts, Aureus II highlights essential market signals such as price movement, market capitalisation, and 24-hour performance. Every detail is presented with clarity and purpose.</p>
+      <p></p>
+      <p></p>
       <p></p>
     </div>
    )
